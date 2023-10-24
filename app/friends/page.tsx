e8d61526860e5fbde94255b10fbe818ac7022b8b
Kalaris Ng <kalaris3255@gmail.com>
2023-10-24 19:50:08 +0800

    Convert module css to tailwind css

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import styles from "./filter.module.css";
 import WhiteFilterToggle from "@/public/whitefilter.png";
 import BlackFilterToggle from "@/public/blackfilter.png";
 import CrossButton from "@/public/crossbutton.png";
@@ -39,14 +38,14 @@ const FriendsPage = () => {
   };
 
   return (
-    <div className="pl-56 pt-5">
-      <div className="flex items-center mb-4">
-        <div className={styles["filter-container"]}>
+    <div className="pl-56 pt-1 mt-7">
+      <div className="flex items-center">
+        <div className="relative mb-5">
           <span className="inline-flex items-center">
             <button
               className={`bg-${
                 isOpen || selectedFilters.length > 0 ? "default-gray" : "white"
-              } pt-2 pb-0.5 pl-6 pr-6 border rounded-3xl`}
+              } pt-1.5 pb-0.5 pl-4 pr-4 border rounded-3xl`}
               onClick={() => setIsOpen(!isOpen)}
             >
               <span className="inline-flex items-center">
@@ -57,22 +56,21 @@ const FriendsPage = () => {
                       : BlackFilterToggle
                   }
                   alt="Logo"
-                  className="w-5 h-6"
+                  className="w-4 h-4"
                 />
                 {selectedFilters.length > 0 && (
-                  <span className="text-white text-base pl-3">
+                  <span className="text-white text-xs pl-3">
                     {selectedFilters.length}
                   </span>
                 )}
               </span>
             </button>
             <span className="pl-5">
-              <Image src={Separator} alt="Logo" className="h-9" />
+              <Image src={Separator} alt="Logo" className="h-7" />
             </span>
-            <span className="pl-5">
+            <span className="pl-5 text-xs">
               <button
                 onClick={() => {
-                  console.log("Clear all button clicked");
                   setSelectedFilters([]);
                   setIsClearAllClicked(true);
                 }}
@@ -80,38 +78,43 @@ const FriendsPage = () => {
                 className={`${
                   selectedFilters.length > 0 && isApplyButtonClicked
                     ? "text-clear-all-blue"
-                    : "text-black"
-                } `}
+                    : "text-gray-300"
+                } font-semibold`}
               >
                 Clear all
               </button>
             </span>
           </span>
           {isOpen && (
-            <div className={styles["dropdown-content"]}>
-              <header className="flex-1 bg-white text-custom-gray font-semibold h-7 w-full flex items-center justify-between shadow-sm">
+            <div className="flex flex-col absolute mt-3 left-0 bg-white border border-white shadow-xl rounded-md text-base w-60">
+              <header className="flex-1 bg-white text-custom-gray font-semibold h-7 w-full flex items-center justify-between shadow-sm p-3">
                 <button
-                  className="pl-2 text-clear-all-blue text-sm"
+                  className={`${
+                    selectedFilters.length > 0
+                      ? "text-clear-all-blue"
+                      : "text-gray-300"
+                  } text-center text-xs`}
                   // clicking on clear all sets to no selected filters
                   onClick={() => {
-                    // handleClearAll;
                     setSelectedFilters([]);
                   }}
                 >
                   Clear all
                 </button>
-                <span className="text-center text-base mr-5">Filter</span>
-                <button className="pr-3 text-2xs" onClick={toggleDropDown}>
+                <span className="text-center text-sm mr-8">Filter</span>
+                <button className="text-xs" onClick={toggleDropDown}>
                   <Image src={CrossButton} alt="X" className="w-3 h-3" />
                 </button>
               </header>
 
-              <div className={styles["filter-category"]}>Friend Status</div>
-              <div className={styles["filter-option"]}>
+              <div className="pt-5 pl-3 text-xs text-gray-600">
+                Friend Status
+              </div>
+              <div className="flex flex-col px-3 justify-between text-gray-700 text-xs font-semibold">
                 {options.map((option, index) => (
-                  <div className={styles["filter-option-item"]} key={index}>
-                    <span className={styles["option-name"]}>{option}</span>
-                    <span className={styles["checkbox-container"]}>
+                  <div className="flex justify-between pt-4" key={index}>
+                    <span>{option}</span>
+                    <span>
                       <input
                         type="checkbox"
                         value={option}
@@ -122,12 +125,11 @@ const FriendsPage = () => {
                   </div>
                 ))}
               </div>
-              <div className="flex items-center justify-center m-1">
+              <div className="flex items-center justify-center m-1 pb-1 pt-5">
                 {/* clicking on apply button applies the filter function to the UI */}
                 <button
-                  className={styles["apply-button"]}
+                  className="flex justify-center items-center w-60 h-8 rounded-md text-white bg-custom-gray text-xs font-semibold"
                   onClick={() => {
-                    console.log("Apply button clicked");
                     setIsApplyButtonClicked(true);
                   }}
                 >
